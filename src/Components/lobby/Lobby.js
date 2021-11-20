01c52bd70a1bf5a071815adbbf452e719571726f
jasonkyl <jasonkyl@J4S0N.localdomain>
2021-11-20 11:25:34 -0500

    fixed css locations, creating makeface page

--- src/Components/lobby/Lobby.js
+++ src/Components/lobby/Lobby.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Lobby.css';
 
 function Lobby() {
     return (
---
+++ src/Components/make_a_face/MakeRoom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MakeRoom() {
+    return (
+        <>
+
+        </>
+    );
+  }
+
+  export default MakeRoom;
