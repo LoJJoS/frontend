55dd89e5b350d39dea47892a60602919d5688685
Lilian <18llc3@queensu.ca>
2021-11-20 20:32:06 -0500

    added files

--- src/Components/guessemoji/GuessEmoji.js
+++ src/Components/guessemoji/GuessEmoji.js
@@ -1,2 +1,36 @@
-import React from 'react';
+import React from "react";
+import 'react-dropdown/style.css';
 import "./GuessEmoji.css";
+
+function GuessEmoji() {
+
+  return (
+    <div className="containerUploadFace">
+      <div className="headerUploadFace">😀 FACE IT</div>
+      <div className="makeThisFaceContainer">
+        <div className="guess-text">Who made this face?</div>
+        <div className="faceContainer">
+          <img
+            className="faceUploadFace"
+            alt="face here"
+            src="https://www.nicepng.com/png/full/5-52286_download-laughing-iphone-emoji-jpg-emoji-happy-png.png"
+          />
+        </div>
+
+        <form classname="dropdown">
+          <select name="Select a name" classname="dropdown-menu">
+            <option value="1">John</option>
+            <option value="2">Jane</option>
+            <option value="3">Jason</option>
+            <option value="4">Jessy</option>
+            <option value="5">James</option>
+          </select>
+        </form>
+
+      </div>
+    </div>
+  );
+}
+  
+export default GuessEmoji;
+  
