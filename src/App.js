b4b4f426d4bdd553aff2d6d953ffda449b422375
Lilian <18llc3@queensu.ca>
2021-11-20 22:42:06 -0500

    import guess emoji pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import JoinRoom from "./Components/joinroom/JoinRoom";
+import Lobby from "./Components/lobby/Lobby";
+import MakeRoom from "./Components/make_a_face/MakeRoom";
+import UploadFace from "./Components/uploadface/UploadFace";
+import GuessEmoji from "./Components/guessemoji/GuessEmoji";
+import GuessingTime from "./Components/guessemoji/GuessingTime";
 
 function App() {
   return (
     <div className="App">
-      <JoinRoom></JoinRoom>
+      {/* <JoinRoom></JoinRoom> */}
+      {/* <Lobby /> */}
+      {/* <MakeRoom /> */}
+      {/* <UploadFace /> */}
+      <GuessEmoji />
+      {/* <GuessingTime /> */}
     </div>
   );
 }
