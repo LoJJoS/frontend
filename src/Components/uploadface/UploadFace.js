20b55a126ace4f40dbdd34a67cde4dfdaa309238
Lilian Chee <71408095+lilchee@users.noreply.github.com>
2021-11-21 05:49:49 -0500

    Update UploadFace.js

--- src/Components/uploadface/UploadFace.js
+++ src/Components/uploadface/UploadFace.js
@@ -4,40 +4,48 @@ import "./UploadFace.css";
 function UploadFace() {
   function handleFile(event) {
     let file = event.target.files[0];
-    var fileExt = file.name.split(".").pop().toLowerCase();
-    window.alert(fileExt);
-    if (fileExt === "jpg" || fileExt === "jpeg" || fileExt === "png") {
-      readFile(file, fileExt);
+    if(file.size > 2097152){
+      alert("File is too big! Limit: 2 MB.");
     } else {
-      window.alert("Wrong file type. Please upload a jpg or png.");
+      var fileExt = file.name.split('.').pop().toLowerCase();
+      if (fileExt === "jpg" || fileExt === "jpeg" || fileExt === "png"){
+        readFile(file, fileExt);
+      } else {
+        window.alert("Wrong file type. Please upload a jpg or png.")
+      }
     }
   }
 
   function readFile(file, fileExt) {
     let myReader = new FileReader();
     myReader.onloadend = function (e) {
-      cb(myReader.result);
+      click1(myReader.result, fileExt);
     };
     myReader.readAsDataURL(file);
+  };
+
+  async function click(base64string, fileExt) {
+    let mydata = {"extension": fileExt, "data": base64string};
+    const response = await fetch("http://34.130.207.147:5000/upload", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(mydata),
+    });
+    return response.json();
   }
 
-  function cb(base64string, fileExt) {
-    /*do next steps here like sending image base64string to the server.*/
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://34.130.207.147:5000/upload", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(
-      JSON.stringify({
-        extension: fileExt,
-        data: base64string,
-      })
-    );
+  function click1() {
+    click().then((data) => processResponse(data));
+  }
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-        alert(xhr.responseText);
-      }
-    };
+  function processResponse(response){
+    // Use this function to go to guess
+    console.log(response);
+    window.alert(response);
   }
 
   return (
@@ -47,29 +55,22 @@ function UploadFace() {
         <div className="label-text">Make this face!</div>
         <div className="faceContainer">
           <img
-            className="emoji"
-            alt="face here"
+            alt="face here" className="face"
             src="https://www.nicepng.com/png/full/5-52286_download-laughing-iphone-emoji-jpg-emoji-happy-png.png"
           />
         </div>
         <div className="buttonsUploadFace">
-          <button className="button takePhoto" id="take-photo">
+          <button
+            className="button takePhoto"
+            id="take-photo"
+            >
             Take a photo
           </button>
 
-          <input
-            type="file"
-            accept="image/*"
-            onChange="handleImageUpload()"
-            id="upload"
-            onChange={handleFile}
-            hidden
-          />
-          <label for="upload" className="button uploadPhoto">
-            Upload a photo
-          </label>
-
-          <img id="display-image" src="" /*TEST*/ />
+          <input type="file" accept="image/*" onChange="handleImageUpload()" id="upload" onChange={handleFile} hidden/>
+          <label for="upload" className="button uploadPhoto">Upload a photo</label>
+          
+          <img id="display-image" src="" /*TEST*//>
         </div>
       </div>
     </div>
